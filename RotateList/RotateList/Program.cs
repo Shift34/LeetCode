97fd3506f66f221777e21fc971e6b895d19628ac
Матвей <matvey.nikolaev.200355@gmail.com>
2024-03-14 20:11:05 +0300

    Решил

--- RotateList/RotateList/Program.cs
+++ RotateList/RotateList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,71 @@ namespace RotateList
     {
         static void Main(string[] args)
         {
+            Solution solution = new Solution();
+            ListNode listNode = new ListNode();
+            ListNode node = new ListNode(0, listNode) ;
+            for (int i = 1 ; i < 2; i++)
+            {
+                listNode.val = i;
+                listNode.next = new ListNode();
+                
+                listNode = listNode.next;
+            }
+            listNode.val = 2;
+            solution.RotateRight(node, 4);
+            Console.ReadKey();
         }
     }
+    public class Solution
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if(head == null)
+            {
+                return head;
+            }
+            int count = 1;
+            ListNode listNode = head;
+            ListNode node = head;
+            while(listNode.next != null)
+            {
+                listNode = listNode.next;
+                count++;
+            }
+            k %= count;
+            if (k == 0)
+            {
+                return head;
+            }
+            for (int i = 0; i<count-k; i++)
+            {
+                node = node.next;
+            }
+            ListNode listNode1 = new ListNode(node.val,node.next);
+            ListNode nod = listNode1;
+            while (nod.next != null)
+            {
+                nod = nod.next;
+            }
+            ListNode node2 = head;
+            for (int i = 0; i < count - k-1; i++)
+            {
+                node2 = node2.next;
+            }
+            node2.next = null;
+            nod.next = head;
+            return listNode1;
+        }
+    }
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+      {
+            this.val = val;
+            this.next = next;
+      }
+    }
 }
+
