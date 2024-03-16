6abdf08befa7bca572a7e97f6ba35579e0f96bba
Матвей <matvey.nikolaev.200355@gmail.com>
2024-03-16 14:25:42 +0300

    решил

---
+++ Two Sum/Two Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> hash = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int different = target - nums[i];
+
+                if (hash.ContainsKey(different))
+                    return new int[] { hash[different], i };
+                else
+                {
+                    if (!hash.ContainsKey(nums[i]))
+                        hash.Add(nums[i], i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
