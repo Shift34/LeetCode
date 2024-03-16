25cf744709a0060cd4c696034c08a4c406c911ca
Матвей <matvey.nikolaev.200355@gmail.com>
2024-03-16 14:33:14 +0300

    решил

---
+++ Palindrome Number/Palindrome Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string text = x.ToString();
+            int y = text.Length - 1;
+            for (int i = 0; i < y; i++, y--)
+            {
+                if (text[i] != text[y])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
